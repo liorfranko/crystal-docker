91d0beb8576cf7221777e5c3f2bc677dd84669c8
liorf <liorf@liveperson.com>
2020-07-30 12:14:08 +0300

    Remove echo

--- crystal-docker.sh
+++ crystal-docker.sh
@@ -1,5 +1,4 @@
 # shellcheck disable=SC2128
 CODE_LOCATION=$(echo "$BASH_SOURCE" | awk -F 'crystal-docker' '{print $1 }')
-echo $CODE_LOCATION
 docker build -t crystal_docker "$CODE_LOCATION/crystal-docker/"
 docker run -it -p 8080:8080 -v "$CODE_LOCATION/crystal-docker/www:/usr/local/src/" crystal_docker
