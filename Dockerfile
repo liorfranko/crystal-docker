de8fc7ae9dc25fe0ab04f99cd0747bd822e3ff3d
liorf <liorf@liveperson.com>
2020-07-30 14:51:10 +0300

    Add ENV to Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,6 @@ RUN curl -L https://keybase.io/crystal/pgp_keys.asc | apt-key add -
 RUN echo "deb https://dist.crystal-lang.org/apt crystal main" > /etc/apt/sources.list.d/crystal.list
 RUN apt-get update
 RUN apt-get install crystal -y
+ENV LISTEN_ADDR="0.0.0.0"
+ENV LISTEN_PORT=8080
 CMD crystal /usr/local/src/home.cr
--- www/home.cr
+++ www/home.cr
@@ -1,15 +1,23 @@
 require "http/server"
-puts "Started http server"
+begin
+    puts "Started http server"
+    ENV["LISTEN_ADDR"] ||= "127.0.0.1"
+    ENV["LISTEN_PORT"] ||= "8080"
+    server = HTTP::Server.new do |context|
+      context.response.content_type = "text/plain"
 
-server = HTTP::Server.new do |context|
-  context.response.content_type = "text/plain"
-  
-  context.response.print "HTTP Path: #{context.request.path}\n"
-  context.response.print "HTTP Body: #{context.request.body}\n"
-  context.response.print "Headers:\n"
-  context.request.headers.each {|x| context.response.print "\n", context.response.print x[1][0], context.response.print ": ", context.response.print x[0], context.response.print "\t" }
+      context.response.print "HTTP Path: #{context.request.path}\n"
+      context.response.print "HTTP Body: #{context.request.body}\n"
+      context.response.print "Headers:\n"
+      context.request.headers.each {|x| context.response.print "\n", context.response.print x[1][0], context.response.print ": ", context.response.print x[0], context.response.print "\t" }
+    end
+
+    address = server.bind_tcp ENV["LISTEN_ADDR"].to_s, ENV["LISTEN_PORT"].to_i
+    server.listen
+    puts "Listening on http://#{ENV["LISTEN_ADDR"].to_s}"
+rescue ex
+    puts "Error: #{ex.message}"
+    puts "#{ex.backtrace}"
+    exit 1
 end
 
-address = server.bind_tcp "0.0.0.0", 8080
-puts "Listening on http://#{address}"
-server.listen
