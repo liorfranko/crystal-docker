4c3c274e98496b004e368d7205fa6e1451d8baf0
liorf <liorf@liveperson.com>
2020-07-30 16:13:35 +0300

    test2

--- crystal-docker.sh
+++ crystal-docker.sh
@@ -2,3 +2,5 @@
 CODE_LOCATION=$(echo "$BASH_SOURCE" | awk -F 'crystal-docker' '{print $1 }')
 docker build -t crystal_docker "$CODE_LOCATION/crystal-docker/"
 docker run -it -p 8080:8080 -v "$CODE_LOCATION/crystal-docker/www:/usr/local/src/" crystal_docker
+
+
