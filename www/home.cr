60cb1ee5d68bc6a2bd2b448ed30def4bdaaba48b
liorf <liorf@liveperson.com>
2020-08-02 19:48:29 +0300

    Fix printing

--- www/home.cr
+++ www/home.cr
@@ -8,17 +8,13 @@ begin
       HTTP::LogHandler.new
     ]) do |context|
       context.response.content_type = "text/plain"
-      # puts "Request path is:"
-      # pp context.request.path
-      # puts "Request body is:"
-      # pp context.request.body
-      # puts "Request header are:"
-      # pp context.request.headers
+      puts "Request path is: #{context.request.path}"
+      puts "Request body is: #{context.request.body}"
+      puts "Request header are: #{context.request.headers}"
       context.response.print "HTTP Path: #{context.request.path}\n"
       context.response.print "HTTP Body: #{context.request.body}\n"
       context.response.print "Headers:\n"
       context.request.headers.each {|x| context.response.print "\n", context.response.print x[1][0], context.response.print ": ", context.response.print x[0], context.response.print "\t" }
-      
     end
 
     address = server.bind_tcp ENV["LISTEN_ADDR"].to_s, ENV["LISTEN_PORT"].to_i
