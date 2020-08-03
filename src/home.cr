dd60606c3865b1db1d30a0bb6712bcf1080e6397
liorf <liorf@liveperson.com>
2020-08-03 18:05:42 +0300

    Fix printing

--- src/home.cr
+++ src/home.cr
@@ -17,15 +17,21 @@ begin
         context.response.content_type = "application/json"
         string = JSON.build do |json|
           json.object do
+            json.field "method", "#{context.request.method}"
             json.field "path", "#{context.request.path}"
             json.field "body", "#{context.request.body}"
+            json.field "query", "#{context.request.query}"
+            json.field "query_params", "#{context.request.query_params}"
             json.field "headers", context.request.headers
           end
         end
         context.response.print string
       else
-        context.response.print "HTTP Path: #{context.request.path}\n"
-        context.response.print "HTTP Body: #{context.request.body}\n"
+        context.response.print "Method: #{context.request.method}\n"
+        context.response.print "Path: #{context.request.path}\n"
+        context.response.print "Body: #{context.request.body}\n"
+        context.response.print "Query: #{context.request.query}\n"
+        context.response.print "Query Params: #{context.request.query_params}\n"
         context.response.print "Headers:\n"
         context.request.headers.each do |header|
           context.response.print "\t"
@@ -35,6 +41,14 @@ begin
             context.response.print h
           end
           context.response.print "\n"
+        end
+        context.response.print "\n"
+        if context.request.headers.has_key?("Host")
+          if ENV["LISTEN_PORT"] == "80"
+            context.response.print "To view it in a \"json\" format use: http://#{context.request.headers.get("Host")[0]}/json"
+          else
+            context.response.print "To view it in a \"json\" format use: http://#{context.request.headers.get("Host")[0]}:#{ENV["LISTEN_PORT"]}/json"
+          end
         end
       end
     end
