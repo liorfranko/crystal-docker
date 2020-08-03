a3316979b3d366799c30f124963b8f74d4186c46
liorf <liorf@liveperson.com>
2020-08-03 19:05:03 +0300

    Change run to push

--- crystal-docker.sh
+++ crystal-docker.sh
@@ -1,5 +1,5 @@
 # shellcheck disable=SC2128
 CODE_LOCATION=$(echo "$BASH_SOURCE" | awk -F 'crystal-docker' '{print $1 }')
 docker build -t liorf1/crystal_docker "$CODE_LOCATION/crystal-docker/"
-docker run -it -p 80:80 liorf1/crystal_docker
-# docker push liorf1/crystal_docker
+# docker run -it -p 80:80 liorf1/crystal_docker
+docker push liorf1/crystal_docker
