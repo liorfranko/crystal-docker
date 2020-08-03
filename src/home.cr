d68fcb188bcd1952e9b6e3a6bf97e2e265bc2e6a
liorf <liorf@liveperson.com>
2020-08-03 17:32:41 +0300

    Remove comments

--- src/home.cr
+++ src/home.cr
@@ -1,11 +1,10 @@
 require "http/server"
 require "json"
-# require "hash"
+ENV["LISTEN_ADDR"] ||= "0.0.0.0"
+ENV["LISTEN_PORT"] ||= "80"
 
 begin
     puts "Starting http server"
-    ENV["LISTEN_ADDR"] ||= "0.0.0.0"
-    ENV["LISTEN_PORT"] ||= "80"
     server = HTTP::Server.new([
       HTTP::ErrorHandler.new,
       HTTP::LogHandler.new
@@ -15,6 +14,7 @@ begin
       when "/health"
         context.response.print "ok"
       when "/json"
+        context.response.content_type = "application/json"
         string = JSON.build do |json|
           json.object do
             json.field "path", "#{context.request.path}"
@@ -23,26 +23,7 @@ begin
           end
         end
         context.response.print string
-        hash = Hash(String, Hash(String, String)).new
-        hash["headers"] = {"test" => "sjdhs"}
-        # context.response.print context.request.headers.to_json
-        context.request.headers.each do |header|
-          # string.test="dasd"
-          # hash["headers"] = {header[0] => header[1][0]}
-          header[1].each do |h|
-            hash["headers"] = {header[0] => h}
-          end
-          # context.response.print header[0]
-          # context.response.print header[1]
-        end
-        # context.response.print "################"
-        # context.response.print hash.to_json
-        # context.response.print "################"
-        # context.response.print context.request.headers.to_json
       else
-        puts "Request path is: #{context.request.path}"
-        puts "Request body is: #{context.request.body}"
-        puts "Request header are: #{context.request.headers.to_pretty_json}"
         context.response.print "HTTP Path: #{context.request.path}\n"
         context.response.print "HTTP Body: #{context.request.body}\n"
         context.response.print "Headers:\n"
@@ -55,7 +36,6 @@ begin
           end
           context.response.print "\n"
         end
-        # {|x| context.response.print "\n", context.response.print x[1][0], context.response.print ": ", context.response.print x[0], context.response.print "\t" }
       end
     end
 
